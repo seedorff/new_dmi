2d65495a8ca2a2f05ddab8993d391444a38f044c
Kristian Brouer Seedorff <seedorff.himself@gmail.com>
2012-11-28 13:53:04 +0100

    basis struktur
    nice

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DMI.dk</title>
+</head>
+<body>
+	<div id="wrapper">
+		
+		<heading class="header">
+			<div id="logo"><img src="" alt="DMI"></div>
+		</heading>
+
+		<div id="content">
+
+		</div>
+
+	</div>
+
+</body>
+</html>
